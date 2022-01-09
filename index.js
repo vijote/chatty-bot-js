aeeb98a2658c6633fb75307d424da170f5e4e30e
Juan Dominguez <jdominguez@simtlix.com>
2022-01-09 13:45:18 -0300

    add connection to hugging face model

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
-const { Client, Intents } = require('discord.js');
-const dotenv = require('dotenv');
+import { Client, Intents } from 'discord.js';
+import fetch from 'node-fetch';
+import * as dotenv from 'dotenv';
 
 dotenv.config()
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+const API_URL = 'https://api-inference.huggingface.co/models/vijote/DialoGPT-small-Morty';
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
+    if (!interaction.isCommand()) return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
 });
+
+client.on('message', async message => {
+    // ignore messages from the bot itself
+    if (message.author.bot) {
+        return;
+    }
+    // form the payload
+    const payload = {
+        inputs: {
+            text: message.content
+        }
+    };
+    // form the request headers with Hugging Face API key
+    const headers = {
+        'Authorization': 'Bearer ' + process.env.HUGGINGFACE_TOKEN
+    };
+
+    // set status to typing
+    message.channel.sendTyping()
+    // query the server
+    const res = await fetch(API_URL, {
+        method: 'post',
+        body: JSON.stringify(payload),
+        headers: headers
+    });
+    const data = await res.json();
+    const botMsg = data.generated_response ? data.generated_response : `I don't know what to say...`;
+    await message.channel.send(botMsg)
+})
 
 client.login(process.env.DISCORD_TOKEN);
