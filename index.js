d4e779a03713969ecf1a649ecfefa7baf3071799
Juan Dominguez <jdominguez@simtlix.com>
2022-01-09 12:54:12 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, Intents } = require('discord.js');
+const dotenv = require('dotenv');
+
+dotenv.config()
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
